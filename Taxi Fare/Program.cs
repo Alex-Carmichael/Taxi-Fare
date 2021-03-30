94521858d81b0856a5759b36d9eebf4499d297d4
Alex Carmichael <33584120+Alex-Carmichael@users.noreply.github.com>
2021-03-30 11:01:23 +0100

    Add files via upload

---
+++ Taxi Fare/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Taxi_Fare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+            int startCost = 3;
+            try
+            {
+
+            
+                Console.Write("Enter the the distance of the journey: ");
+                int distance = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter number of passengers: ");
+                int passengers = Convert.ToInt32(Console.ReadLine());
+
+                if (distance > 1) 
+                {
+                    distance = distance - 1;
+                }
+            
+
+                double cost = distance * 2;
+                cost = cost + startCost;
+                
+                if (passengers >= 5)
+                {
+                    cost = cost * 1.5;
+                }
+                Console.WriteLine("The final cost is "+ cost);
+                
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("This is not a valid input");
+            }
+
+            }
+        }
+    }
+}
